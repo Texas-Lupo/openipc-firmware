59e98ffd89f806a81bcf5b65f4480e49b17f9663
Texas-Lupo <vulpisfoglia.anon@gmail.com>
2026-03-27 09:19:35 +0200

    Update fluke.mk

--- general/package/fluke/fluke.mk
+++ general/package/fluke/fluke.mk
@@ -5,7 +5,7 @@
 ################################################################################
 
 # Use the specific commit hash or branch name (e.g., master)
-FLUKE_VERSION = bc83435beb34abf3c647c11b5cdebb8e85906bfd
+FLUKE_VERSION = 3fdd9dca080a530b5b89b3ad054a0b985e97a207
 FLUKE_SITE = $(call github,Texas-Lupo,fluke,$(FLUKE_VERSION))
 
 FLUKE_LICENSE = GPL-3.0
